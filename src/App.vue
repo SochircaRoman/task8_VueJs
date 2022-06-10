6933f299f154df0b01685b8837b5f41960f0b615
User01 <romansochirca30@gmail.com>
2022-06-10 09:40:25 +0300

    add vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import ListPersons from './components/ListPersons.vue';
+
+export default {
+    components: { ListPersons },
+    data() {
+        return {
+            personInformation: {},
+        }
+    },
+    methods: {
+        
+    },
+
+
+}
+
+</script>
+
+<template>
+
+    <div class="app">
+        <list-persons @emitPersonInformation="emitPersonInformation"></list-persons>
+        <details-persons :personInformation="personInformation"></details-persons>
+    </div>
+
+
+  
+</template>
+
+<style scoped>
+
+.app{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
---
+++ src/components/ListPersons.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="wraper">
+            <div class="element">
+                <label for="name"><strong>Name:</strong></label>
+                <span id="name"><i>{{ ` ${personArr.name}` }}</i></span>
+            </div>
+            <div class="element">
+                <label for="phone"><strong>Phone:</strong> </label>
+                <span id="phone"><i>{{ ` ${personArr.user}` }}</i></span>
+            </div>
+            <div class="element">
+                <label for="email"><strong>Email:</strong></label>
+                <span id="email"><i>{{ ` ${personArr.admin}` }}</i></span>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            personsArr: [
+                {name: "Name", user: "Sochirca Roman", admin: "Sochirca Roman"},
+            ],
+        }
+    },
+    methods: {
+        
+}
+}
+</script>
+
+<style scoped>
+
+.wraper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:flex-start;
+    margin-top: 50px;
+    margin-right: 50px;
+}
+
+.element {
+    border: 1px solid #000;
+    padding: 15px;
+    width: 170px;
+    cursor: pointer;
+}
+
+.element:hover {
+  border: 3px solid aquamarine;
+}
+
+
+</style>
